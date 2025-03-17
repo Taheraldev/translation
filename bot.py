e0e5d2eb42dcbfd4c0924ec370f835d685b30f56
Taheraldev <101641519+Taheraldev@users.noreply.github.com>
2025-03-17 11:56:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,28 @@
 import time
+
 import groupdocs_translation_cloud
-from groupdocs_translation_cloud.models import FileRequest, Format, SavingMode  # تعديل هنا
+from groupdocs_translation_cloud import FileRequest, Format
 
 api = groupdocs_translation_cloud.api.TranslationApi()
 file_api = groupdocs_translation_cloud.api.FileApi()
 api.api_client.configuration.client_id = "a91a6ad1-7637-4e65-b793-41af55450807"
 api.api_client.configuration.client_secret = "2d0c949f2cc2d12010f5427e6c1dc4da"
 
-url = file_api.file_upload_post(file="/path/to/yourfile.docx", format=Format.Docx)
+url = file_api.file_upload_post(file="yourfile.docx", format=Format.Docx)
 file_request = FileRequest(source_language="en", 
-                           target_languages=["ru"], 
-                           url=url, 
-                           format=Format.Docx,
-                           saving_mode=SavingMode.Files, 
-                           output_format=Format.Docx)
+							           target_languages=["ru"], 
+							           url=url, 
+							           format=Format.Docx,
+							           saving_mode=SavingMode.Files, 
+							           output_format=Format.Docx)
 response = api.auto_post(file_request)
 if response.status == 202:
-    request_id = response.request_id  # تأكد من استخراج request_id بالشكل الصحيح
     while True:
         file_response = api.document_request_id_get(request_id)
         if file_response.status == 200:
             print(file_response.message)
             for lang in file_response.urls:
-                print(lang + '_' + url + ': ' + file_response.urls[lang].url)
+                print(lang + '_' + url + ': ' + file_response.urls[
+                    lang].url)
             break
         time.sleep(2)
